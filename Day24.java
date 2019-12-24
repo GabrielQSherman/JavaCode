319396c208566bab84f7f0efffdfd60f3b3ed17f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2019-12-23 23:56:50 -0500

    linked list practice

--- Day24.java
+++ Day24.java
@@ -13,6 +13,15 @@ class Solution
 {
 
     public static Node removeDuplicates(Node head) {
+
+        Node curr = head;
+        while (curr != null && curr.next != null) {
+            while (curr.next != null && curr.data == curr.next.data) {
+                curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return head;
         
 
     }
