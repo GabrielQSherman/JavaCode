97f48001140846ef675ed7df89ce57fa4054a0b1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2019-12-26 10:13:48 -0500

    Prime finder complete

--- Day25.java
+++ Day25.java
@@ -7,8 +7,18 @@ import java.util.regex.*;
 public class Main {
     
     public static String isPrime(int num) {
-        for (int i = 2; i < num - 1; i++) { //this insures that one and the number itself are not counted. the sqrt of the number would be a better number to end with
-            if (num % i == 0) { //logic that finds a non-prime number
+
+        if (num == 1){
+            return "Not prime"; //one is not prime
+        }
+
+        double dubnum = num;
+        dubnum = Math.ceil(Math.sqrt(dubnum)); //this is to create more effecenit code my stopping the loop after all the possible factors of 'num' have been tested
+        
+        int sqrt = (int)dubnum;
+
+        for (int i = 2; i < sqrt; i++) { //this insures that one and the number itself are not counted. the sqrt of the number would be a better number to end with
+            if (num % i == 0 && num != i) { //logic that finds a non-prime number
                 return "Not Prime"; //the loop will end if at any point the number is proven that a number other than one and itself can divide it
             }
         } 
