497a7d4f40975c465d2d78551b65fcbccad9694d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2019-12-26 09:31:10 -0500

    created a loop that returns if a given number is prime

--- Day25.java
+++ Day25.java
@@ -7,7 +7,12 @@ import java.util.regex.*;
 public class Main {
     
     public static String isPrime(int num) {
-        return "The number " + num;
+        for (int i = 2; i < num - 1; i++) { //this insures that one and the number itself are not counted. the sqrt of the number would be a better number to end with
+            if (num % i == 0) { //logic that finds a non-prime number
+                return "Not Prime"; //the loop will end if at any point the number is proven that a number other than one and itself can divide it
+            }
+        } 
+        return "Prime"; //if it makes it through the whole loop than it must be prime
     }
 
     public static void main(String[] args) {
@@ -22,10 +27,5 @@ public class Main {
             num = sc.nextInt();
         }
         
-        
-        
     }
 }
-
-
-
