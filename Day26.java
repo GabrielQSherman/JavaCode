7e2a0da2e5a39dcf8f5c20fc6a93500b334cb80d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2019-12-27 12:38:20 -0500

    non-nested logic working

--- Day26.java
+++ Day26.java
@@ -7,24 +7,42 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your            lass should be named Solution. */
-
-       Scanner scan = new Scanner(Systen.in);
-
-       List<Integer> returnedDate = new ArrayList<Integer>();
-       for (int i = 0; i < 3; i++) {
-        returnedDate.add(scan.nextInt());
-       }
-
-       List<Integer> lateDate = new ArrayList<Integer>();
-       for (int i = 0; i < 3; i++) {
-        lateDate.add(scan.nextInt());
-       }
-
         
-
-       scan.close();
-
+       Scanner scan = new Scanner(System.in);
+       
+       int[] returnedDate = new int[3];
+      
+    //   List<Integer>   = new ArrayList<Integer>();
+       for (int i = 0; i < 3; i++) {
+        // returnedDate.add(scan.nextInt());
+        returnedDate[i] = scan.nextInt();
+       }
+        
+         int[] lateDate = new int[3];
+    //   List<Integer> lateDate = new ArrayList<Integer>();
+       for (int i = 0; i < 3; i++) {
+        // lateDate.add(scan.nextInt());
+        lateDate[i] = scan.nextInt();
+       }
+       
+       int days = (returnedDate[0] - lateDate[0]);
+       int months = (returnedDate[1] - lateDate[1]);
+       int years = (returnedDate[2] - lateDate[2]);
+       
+       
+       if (years > 0){
+           System.out.println("10000");
+           return;
+       } else if (months > 0) {
+            System.out.println(months * 500);
+            return;
+        } else if (days > 0) {
+            System.out.println(days * 15);
+            return;
+        } else {
+            System.out.println("0");
+            return;
+        }
     }
 }
 
